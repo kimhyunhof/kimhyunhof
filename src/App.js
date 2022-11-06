db8b5e10aaea1549d5123c963ffce91091de1d1c
kim <komppang@naver.com>
2022-11-06 17:31:00 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+import Customer from'./components/Customer'
+
+
+
+
+const coustomer = {
+
+'name' : '홍길동';
+'birth' : '34045';
+
+
+
+}
+
+
+class App extends Component{
+
+render(){
+return(
+
+<Customer
+
+name = {}
+birthday ={}
+gender={}
+job={}
+/>
+
+
+)
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
+
+export default App;
